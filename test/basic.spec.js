4662d324f8201011177df73ee817bb87308e5b84
al66 <andreas.leinen@imicros.de>
2021-10-31 09:49:06 +0100

    next try to fix import problem

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// import { Modeler } from "../dist/main.js";
-const { Modeler } = require("../index.js");
+import { Modeler } from "../index.js";
 
 describe("Test Basic Modeler", () => {
 
     it("should create a new instance", () => {
+        console.log(Modeler);
         let modeler = new Modeler(); 
         expect(test).toBeDefined();
         expect(modeler).toBeDefined();
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,9 +2,25 @@ const path = require("path");
 
 module.exports = {
     mode: "none",
-    entry: "./lib/index.js",
+    entry: "./index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }      
 };
